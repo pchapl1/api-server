f0b8fae3f08b73d52afeb13ad32ede24a27226b4
Phil Chaplin <pchapl1@gmail.com>
2023-04-06 09:09:57 -0700

    added yml file

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,7 +1,8 @@
 const server = require('../src/server');
 const supertest = require('supertest');
 const request = supertest(server.app);
-const {sequelize} = require('../src/models/index')
+const {sequelize} = require('../src/models/index');
+const e = require('express');
 
 // have to initialize the db before the tests
 beforeAll(async()=>{
@@ -18,14 +19,31 @@ describe('GET /nonexistent', () => {
         const response = await request.get('/nonexistent');
         expect(response.status).toBe(404);
     });
+
+    test('get allFood should return 200', async () => {
+
+        const response = await request.get('/food');
+        expect(response.status).toEqual(200);
+    })
+
+    test('get 1 food should return 200', async () => {
+
+        const response = await request.get('/food/1');
+        expect(response.status).toEqual(200);
+    })
     
-    test('should return 200', async () => {
+    test('create food should return 200', async () => {
         let mockObj = {
             name: "chicken",
             category: "meat"
         }
         const response = await request.post('/food').send(mockObj)
-        console.log(response)
         expect(response.status).toEqual(200);
     })
+
+    test('update food should return 200', async ()=> {
+        
+        const response = await request.put('/food/1')
+    })
+
 });
